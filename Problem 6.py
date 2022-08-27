e847deb98a8f99d4b90f92aace7514eb5179b5da
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-08-27 05:18:41 -0500

    Update Problem 6.py

--- Problem 6.py
+++ Problem 6.py
@@ -7,7 +7,7 @@ pensize(4)
 
 #drawing the shape
 for i in range (10):
-    for p in range(5):
-        right(72)
+    for p in range(5): #for p in range(6)- hexagon has six sides
+        right(72)      #right(60) <= 360/6 = 60
         forward(100)
-    right(45)
+    right(45)  #right(36) <==  360/10
